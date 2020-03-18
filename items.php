9e0a23eebb05d6e668f8fd4a0b7a762eca6aab74
Brian Emilius <brian@brianemilius.com>
2020-03-18 19:09:57 +0100

    add id to items

--- items.php
+++ items.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json");
 $items = [];
 
 $feta = new stdClass();
+$feta->id = 1;
 $feta->name = "Feta";
 $feta->price = 42.5;
 $feta->description = "Verdens bedste ost er feta.";
@@ -11,6 +12,7 @@ $feta->image = "feta.jpg";
 $items[] = $feta;
 
 $gouda = new stdClass();
+$gouda->id = 2;
 $gouda->name = "Gouda";
 $gouda->price = 56.25;
 $gouda->description = "Hollandsk ost af den bedste slags.";
@@ -18,6 +20,7 @@ $gouda->image = "gouda.jpg";
 $items[] = $gouda;
 
 $brie = new stdClass();
+$brie->id = 3;
 $brie->name = "Brie";
 $brie->price = 35;
 $brie->description = "Blød og cremet med et lækkert bid og en tyk skorpe.";
@@ -25,6 +28,7 @@ $brie->image = "brie.jpg";
 $items[] = $brie;
 
 $danbo = new stdClass();
+$danbo->id = 4;
 $danbo->name = "Danbo";
 $danbo->price = 52.95;
 $danbo->description = "Hård ekstralagret Danbo fra Vesterhavet. Med salt og kommen.";
